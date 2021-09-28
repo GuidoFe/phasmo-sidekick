a8bf2ee7e4175fb957ec929a77f0b689efcdf20c
GuidoFe <guidoferris@gmail.com>
2021-09-28 21:45:07 +0200

    Changed bot activity to 'Playing !ph'

--- test/help.spec.js
+++ test/help.spec.js
@@ -11,18 +11,18 @@ describe('command help', function() {
         dataManager.constants = {prefix: '!ph'};
         help = new Help(dataManager);
         help.init({
-            'commandA': {shortDescription: 'Short description A', longDescription: 'Long description A'},
-            'commandB': {shortDescription: 'Short description B', longDescription: 'Long description B'},
+            'commandA': {shortDescription: 'Short description A', longDescription: 'Long description A', commandUsage: '!ph A'},
+            'commandB': {shortDescription: 'Short description B', longDescription: 'Long description B', commandUsage: '!ph B'},
         });
     });
     it('should have a non-empty command list', function() {
-        assert.ok(help.commandList && Object.keys(help.commandList).length == 2, `Command list:\n${Object.keys(help.commandList)}`);
+        assert.ok(help.commandList && Object.keys(help.commandList).length == 3, `Command list:\n${Object.keys(help.commandList)}`);
     });
     it('should return the command B help message', function() {
         const message = new MessageStub(`!ph help commandB`);
         const result = help.execute(message);
         assert.equal(result, 0);
-        assert.equal(testUtils.getEmbedMessageDescription(message.response), 'Long description B', message.response);
+        assert.equal(message.response.embeds[0].fields[0].value, 'Long description B');
     });
     it('should not accept invalid command name', function() {
         const message = new MessageStub(`!ph help ewfdsaf`);
@@ -33,10 +33,8 @@ describe('command help', function() {
         const message = new MessageStub(`!ph help`);
         const result = help.execute(message);
         assert.equal(result, 0, `Returned ${result}`);
-        const response = testUtils.getEmbedMessageDescription(message.response);
-        assert.ok(response, 'Response is not defined.');
-        const nNewLines = (response.match(/\n/g)||[]).length;
-        assert.ok(nNewLines > 2, response);
+        const nCommands = message.response.embeds[0].fields.length;
+        assert.equal(nCommands, Object.keys(help.commandList).length);
     });
 });
 
