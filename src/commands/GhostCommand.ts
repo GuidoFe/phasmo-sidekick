14586e3f0e6dc21761f57e5484e9463b1ea50041
GuidoFe <guidoferris@gmail.com>
2021-11-25 23:10:50 +0100

    Added ghost icon as attachment

--- src/commands/GhostCommand.ts
+++ src/commands/GhostCommand.ts
@@ -1,7 +1,7 @@
 import {SlashCommandBuilder} from '@discordjs/builders';
 import {SlashCommand, DataManager, Ghost} from '@modules';
 import utils = require('@utils');
-import {CommandInteraction, MessageEmbed} from 'discord.js';
+import {CommandInteraction, MessageEmbed, MessageAttachment} from 'discord.js';
 import {ghosts} from '@ghosts';
 import fs = require('fs');
 
@@ -33,6 +33,7 @@ export class GhostCommand extends SlashCommand {
         const embed = new MessageEmbed()
             .setTitle(ghost.name)
         let clues: {name: string, value: string, inline: boolean}[] = []
+        const attachment = new MessageAttachment(`../res/images/ghosts/ghost${utils.getRandomInt(this.nThumbnails)}.png`, "ghostie")
         ghost.clues.forEach((value, index) => clues.push({name: index == 0 ? "Evidence" : "\u200b", value: `${this.dataManager.constants.clueEmoji[value]} ${this.dataManager.constants.clueNames[value]}`, inline: true}))
         embed.setFields([...clues,
                          {name: "Description", value: ghost.description, inline: false},
@@ -41,11 +42,12 @@ export class GhostCommand extends SlashCommand {
                          {name: "Origin", value: ghost.flag ? `${ghost.flag} ${ghost.origin}` : ghost.origin, inline: false}
         ])
         embed.setColor(utils.randomVibrantColor())
+        embed.setThumbnail('attachment://ghostie')
         //embed.setDescription(`**Origin**\n\n${ghost.flag ? `${ghost.flag} ${ghost.origin}` : ghost.origin}`)
         embed.setFooter("From Phasmophobia Wiki, Wikipedia and Oxford Languages.")
-        const thumbnailUrl = `https://${process.env.RAILWAY_STATIC_URL}/res/images/ghosts/ghost${utils.getRandomInt(this.nThumbnails)}.png`
-        console.log(thumbnailUrl)
-        embed.setThumbnail(thumbnailUrl)
+//        const thumbnailUrl = `https://${process.env.RAILWAY_STATIC_URL}/res/images/ghosts/ghost${utils.getRandomInt(this.nThumbnails)}.png`
+ //       embed.setThumbnail(thumbnailUrl)
+
         await interaction.reply({embeds: [embed]})
     };
 };
