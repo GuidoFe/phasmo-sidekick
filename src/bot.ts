f104c6618ddd6797a23645c20e58d363ce46b30e
GuidoFe <guidoferris@gmail.com>
2022-01-15 15:26:34 +0100

    Update bot.ts

--- src/bot.ts
+++ src/bot.ts
@@ -64,6 +64,6 @@ client.on('interactionCreate', async (interaction) => {
     commandManager.run(interaction.commandName, interaction)
 });
 client.on('guildCreate', (guild) => {
-    utils.sendLogMessage(`Joined new Guild: ${guild.name} (${guild.memberCount})\nDescription: ${guild.description}\n${guild.iconURL({dynamic: true, size: 2048})}`);
+    //utils.sendLogMessage(`Joined new Guild: ${guild.name} (${guild.memberCount})\nDescription: ${guild.description}\n${guild.iconURL({dynamic: true, size: 2048})}`);
     console.log(`Joined ${guild.name}`);
 });
